9b30d43e93d193be932001fe24a47f0d399fff95
Tomonori Matsumura <matsumura_t@yahoo.co.jp>
2026-08-15 18:06:49 +0900

    バージョン表示処理を簡素化した refs #5215

--- src/scripts/show-version.sh
+++ src/scripts/show-version.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-bandit --version 2>&1 \
-| GREP_COLORS='mt=01;34' grep -E --color=always '[[:digit:]]|^' \
-| GREP_COLORS='mt=01;33' grep -E --color=always 'bandit|^'
+set -eo pipefail
+
+bandit --version
